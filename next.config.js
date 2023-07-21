6f7e68152093d21ab91aef7b3e9cdff1bf421071
mrafei <m.rafei96@gmail.com>
2023-07-21 14:11:34 +0330

    remove light mode

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-const shouldAnalyzeBundles = process.env.ANALYZE === "true";
+const shouldAnalyzeBundles =
+  process.env.ANALYZE === "true" && process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 let nextConfig = {};
 
 if (shouldAnalyzeBundles) {
-  const withNextBundleAnalyzer = require("next-bundle-analyzer")({enabled: true});
+  const withNextBundleAnalyzer = require("next-bundle-analyzer")({
+    enabled: true,
+  });
   nextConfig = withNextBundleAnalyzer(nextConfig);
 }
 
